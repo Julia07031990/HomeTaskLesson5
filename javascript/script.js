57d7507a839fcfd6c8fdcf7d83139e65bd308456
Skathy <chibikstrong@gmail.com>
2020-03-17 12:36:35 +0200

    some functionality added

--- javascript/script.js
+++ javascript/script.js
@@ -8,3 +8,29 @@ document.getElementById("sentMessage").onclick = function(e) {
 document.getElementById("follow").onclick = function(e) {
 	alert("You are follower now!");
 }
+
+document.getElementById('messageArea').onclick = function(event) {
+	var target = event.target;
+	if (target.className == 'hideBar') {
+		var subm = document.getElementsByClassName("messageWrap");
+		for (var i = 0; i < subm.length; i++) {
+		subm[i].style.display = 'block';
+	}
+}
+} 
+
+document.onclick = function(event){
+	var target = event.target;
+	if (target.className == 'closeMenu') {
+		closeMenu();
+	}
+}
+
+function closeMenu () {
+	var textArea = document.getElementById('messageArea');
+	var subm = document.getElementsByClassName("messageWrap");
+	for (var i = 0; i < subm.length; i++) {
+		subm[i].style.display = 'none';
+	}
+
+}
